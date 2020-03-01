5d8ce44a71d9ff3af30f99c2ad629a5e5dfaeb53
Tanvir <hi.tanin@gmail.com>
2020-03-01 17:03:47 +0100

    Initial Project Setup

---
+++ src/main/java/com/spring/datafetcher/DataFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.spring.datafetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFetcherApplication.class, args);
+	}
+
+}
